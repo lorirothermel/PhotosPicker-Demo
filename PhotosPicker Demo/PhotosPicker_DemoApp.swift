adbcde749a1047e6cde123d2d52e7d80698ae789
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-07 13:18:37 -0400

    Initial Commit

---
+++ PhotosPicker Demo/PhotosPicker_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPicker_DemoApp.swift
+//  PhotosPicker Demo
+//
+//  Created by Lori Rothermel on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPicker_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
