b4034dbe93589d914370bc0f395959bf74ced602
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-08 00:01:30 -0400

    New PhotosPicker Demo

--- PhotosPicker Demo/ContentView.swift
+++ PhotosPicker Demo/ContentView.swift
@@ -6,18 +6,42 @@
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedPhotoData: Data?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            if let selectedPhotoData, let image = UIImage(data: selectedPhotoData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+            }  // if statements
+        
+        PhotosPicker(selection: $selectedItem, matching: .images) {
+            Label("Select a Photo", systemImage: "photo")
+        }  // PhotosPicker
+        .tint(.purple)
+        .controlSize(.large)
+        .buttonBorderShape(.roundedRectangle(radius: 5))
+        .buttonStyle(.borderedProminent)
+        .onChange(of: selectedItem) { newItem in
+            Task {
+                if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                    selectedPhotoData = data
+                }  // if let data
+            }  // Task
+        }  // onChange
+        }  // ZStack
+    }  // some View
+}  // ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
